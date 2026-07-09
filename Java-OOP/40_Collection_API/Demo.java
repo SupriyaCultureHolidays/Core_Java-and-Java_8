3b3fe3f387d9867e20a267438220393c13620ff8
SupriyaCultureHolidays <supriya.cultureholidays@gmail.com>
2026-07-09 21:36:19 +0530

    add

--- Java-OOP/40_Collection_API/Demo.java
+++ Java-OOP/40_Collection_API/Demo.java
@@ -1,3 +1,4 @@
+
 /*
  * Question 39 (39_Collection_API) — Collection Overview
  *
@@ -7,8 +8,28 @@
  * Output:
  * [Rahim, Karim, Sumi]
  */
+import java.util.ArrayList;
+import java.util.List;
 public class Demo {
     public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        //add new element
+        names.add("Rahim");
+        names.add("Karim");
+        names.add("Sumi");
+
+        // replace or update exiting element
+        names.add(1,"Supriya");
+        names.set(3, "Anima");
+
+        // get and length
+        System.out.println(names.get(3));
+        System.out.println(names.size());
+        System.out.println(names.contains("Ani"));
+        System.out.println("Anima".startsWith("Ani")); // vv for knowledge
+        System.out.println(names.remove(3));
+
+        System.out.println(names);
 
     }
 }
