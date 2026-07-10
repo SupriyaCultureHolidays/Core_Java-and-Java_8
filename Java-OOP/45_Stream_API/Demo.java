d78ce316948dd78c8600195f33183b54b9c47c9b
SupriyaCultureHolidays <supriya.cultureholidays@gmail.com>
2026-07-10 11:14:58 +0530

    add

--- Java-OOP/45_Stream_API/Demo.java
+++ Java-OOP/45_Stream_API/Demo.java
@@ -7,8 +7,23 @@
  * Output:
  * 1 2 3 4 5
  */
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo {
     public static void main(String[] args) {
-
+        List<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.add(5);
+        System.out.println(arr);
+        // Stream Api
+        List<Integer> maxnumber = arr.stream().
+        filter((m)->{
+            return m==5;
+        });
     }
 }
